b3ae920c2a8997543f9fd5042cc132fb32427008
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2021-02-02 00:01:07 -0200

    Adicionado geolocalização dos barbeiros

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -95,10 +95,49 @@ class BarberController extends Controller
         }
     */
 
+    private function getGeo($address){
+        $key = env('MAPS_KEY', null);
+        $address = urlencode($address);
+        $url = 'https://maps.googleapis.com/maps/api/geocode/json?address='.$address.'&key='.$key;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($res, true);
+    }
+
     public function list(Request $request){
         $array = ['error' => ''];
 
-        $barbers = Barber::all();
+        $lat = $request->input('lat');
+        $lng = $request->input('lng');
+        $city = $request->input('city');
+
+        if(!empty($city)){
+            $res = $this->getGeo($city);
+            if(count($res['results']) > 0){
+                $lat = $res['results'][0]['geometry']['location']['lat'];
+                $lng = $res['results'][0]['geometry']['location']['lng'];
+            }
+        }elseif(!empty($lat) && !empty($lng)){
+            $res = $this->getGeo($lat.','.$lng);
+            if(count($res['results']) > 0){
+                $city = $res['results'][0]['formatted_address'];
+            }
+        }else{
+            $lat = '-23.5630807';
+            $lng = '-46.6682895';
+            $city = 'São Paulo';
+        }
+
+        $barbers = Barber::select(Barber::raw('*, SQRT(
+            POW(69.1 * (latitude - '.$lat.'), 2) +
+            POW(69.1 * ('.$lng.' - longitude) * COS(latitude / 57.3), 2)) AS distance'))
+            ->havingRaw('distance < ?', [10])
+            ->orderBy('distance', 'ASC')
+            ->get();
 
         foreach($barbers as $bkey => $bValue){
             $barbers[$bkey]['avatar'] = url('media/avatars/'. $barbers[$bkey]['avatar']);
