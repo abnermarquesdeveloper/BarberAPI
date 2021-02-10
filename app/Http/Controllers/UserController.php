b5f541af6a79d7c54fb86ecfd9b8c2c6589a9d2e
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2021-02-09 23:36:25 -0200

    Favoritar um barbeiro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\Barber;
+use App\Models\UserFavorite;
+
 class UserController extends Controller
 {
     private $loggedUser;
@@ -21,6 +24,35 @@ class UserController extends Controller
         $array['avatar'] = url('media/avatars/'.$info['avatar']);
         $array['data'] = $info;
 
+
+        return $array;
+    }
+
+    public function toggleFavorite(Request $request){
+        $array = ['error'=>''];
+
+        $id_barber = $request->input('barber');
+
+        $barber = Barber::find($id_barber);
+
+        if($barber){
+            $fav = UserFavorite::select()
+                ->where('id_user', $this->loggedUser->id)
+                ->where('id_barber', $id_barber)
+                ->first();
+            if($fav){
+                $fav->delete();
+                $array['have'] = false;
+            }else{
+                $newFav = new UserFavorite();
+                $newFav->id_user = $this->loggedUser->id;
+                $newFav->id_barber = $id_barber;
+                $newFav->save();
+                $array['have'] = true;
+            }
+        }else{
+            $array['error'] = 'Barbeiro inexistente!';
+        }
 
         return $array;
     }
